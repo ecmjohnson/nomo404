70792081beeded435ba98061b4ea99f0c4e41c6c
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-21 23:04:52 -0400

    Passes demo tests

--- MyBoiInti/inti.py
+++ MyBoiInti/inti.py
@@ -5,14 +5,14 @@ def rprime(a,b):
         a,b = b, a%b
     return a == 1
 
-for case in range(0, test_cases):
-    temp = raw_input("")
-    N = int(temp.split("")[0])
-    A = int(temp.split("")[1])
-    B = int(temp.split("")[2])
+for case in range(0, int(test_cases)):
+    temp = input("")
+    N = int(temp.split(" ")[0])
+    A = int(temp.split(" ")[1])
+    B = int(temp.split(" ")[2])
 
     accum = 0
-    for K in range(A+1, B+1):
+    for K in range(A, B+1):
         if rprime(N,K):
             accum += K
 
