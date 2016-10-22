eefcc480be63f729ebc0edfb8dc37359a7dafc39
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-21 21:48:36 -0400

    Started working on the inti problem

---
+++ MyBoiInti/inti.py
@@ -0,0 +1,9 @@
+test_cases = raw_input("")
+
+for case in range(0, test_cases):
+    temp = raw_input("")
+    N = int(temp.split("")[0])
+    A = int(temp.split("")[1])
+    B = int(temp.split("")[2])
+
+    
