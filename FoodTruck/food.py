32732830c8db4e8ed1cbabef67a1b33d93726f66
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-22 00:54:25 -0400

    Passing food truck partial solution

--- FoodTruck/food.py
+++ FoodTruck/food.py
@@ -2,7 +2,7 @@ import math
 def Haversine(lat1, long1, lat2, long2):
     sin2 = math.pow(math.sin((lat1-lat2)/2),2)
     cos2sin2 = math.cos(lat1)*math.cos(lat2)*math.pow(math.sin((long1-long2)/2),2)
-    return 2*6378137*math.asin(sin2+cos2sin2)
+    return 2*6378.137*math.asin(sin2+cos2sin2)
 
 # now do the actual problem
 madhu_location = input("")
@@ -13,14 +13,21 @@ max_dist = float(input(""))
 temp = input("")
 last_dist = 0
 subs = []
-while last_dist < max_dist:
-    temp = input("").split(",")
+while True:
+    try:
+        temp = input("").split(",")
+    except EOFError:
+        break
     s_lat = float(temp[1])
     s_long = float(temp[2])
     s_num = temp[3]
     last_dist = Haversine(m_lat, m_long, s_lat, s_long)
     if last_dist < max_dist:
-        subs.append(s_num)
+        subs.append(str(s_num))
 
-output = ",".join(s_num)
+output = list(set(subs))
+output = list(map(int, output))
+output.sort()
+output = list(map(str, output))
+output = ",".join(output)
 print(output)
