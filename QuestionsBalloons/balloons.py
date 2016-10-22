94c669fee1d812113e8c7bf7cb86ca0afc19ca43
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-21 20:34:38 -0400

    Made it not die completely

--- QuestionsBalloons/balloons.py
+++ QuestionsBalloons/balloons.py
@@ -4,3 +4,5 @@ for case in range(0, text_cases):
     test_info = raw_input("")
     total_qs = int(test_info.split(" ")[0])
     lies = int(test_info.split(" ")[1])
+        for q in range(0, total_qs):
+            pass
