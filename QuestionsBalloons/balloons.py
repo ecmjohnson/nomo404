ae77118ce16fac659d54650e9f59afd976a8feaa
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-21 20:32:44 -0400

    Updated to total questions

--- QuestionsBalloons/balloons.py
+++ QuestionsBalloons/balloons.py
@@ -2,5 +2,5 @@ test_cases =  int(raw_input(""))
 
 for case in range(0, text_cases):
     test_info = raw_input("")
-    truths = int(test_info.split(" ")[0])
+    total_qs = int(test_info.split(" ")[0])
     lies = int(test_info.split(" ")[1])
