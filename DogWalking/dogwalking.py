83eb30d26cc668fc6dbcaa8f10a8c8b7060024da
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-22 06:03:34 -0400

    Well, that didn't last long...

--- DogWalking/dogwalking.py
+++ DogWalking/dogwalking.py
@@ -9,35 +9,15 @@ for i in range (0, tests):
         myDawgs.append(int (raw_input("")))
 
     myDawgs.sort(reverse=True)
-    flag = True
 
+    # original divying up of dogs
     walkers = [0]*(N-1)
     for k in range(0,N-1):
-        if flag:
-            walkers[k] = myDawgs[0]
-            del myDawgs[0]
-            flag = False
-        else:
-            walkers[k] = myDawgs[-1]
-            del myDawgs[-1]
-            flag = True
+        walkers[k] = myDawgs[0]
+        del myDawgs[0]
 
-    walkers.sort(reverse=False)
-    while len(myDawgs)>0:
-        last_diff = False
-        for k in range(0,N-1):
-            diff = abs(walkers[k] - myDawgs[0])
-            if not last_diff:
-                last_diff = diff
-            if last_diff < diff:
-                walkers[k-1].append(myDawgs[0])
-                del myDawgs[0]
-                break
-
-    dawg_sizing = 0
-    for walker in walkers:
-        if isinstance(walker, (int, long)):
+    # extra dogs must get owners
+    for i in range(0, len(myDawgs)):
+        diffs = []
+        for j in range(0, len(walkers)):
             pass
-        else:
-            dawg_sizing += max(walker) - min(walker)
-    print(dawg_sizing)
