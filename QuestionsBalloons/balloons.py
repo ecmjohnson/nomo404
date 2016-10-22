cfc69479f86ddc850e5ff6f856758e75937c8f41
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-10-21 20:32:03 -0400

    Starting the balloons problem

---
+++ QuestionsBalloons/balloons.py
@@ -0,0 +1,6 @@
+test_cases =  int(raw_input(""))
+
+for case in range(0, text_cases):
+    test_info = raw_input("")
+    truths = int(test_info.split(" ")[0])
+    lies = int(test_info.split(" ")[1])
